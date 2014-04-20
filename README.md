b6d407717ec33fffe02d2fca3b7a6d8ba51d8732
Ramon Lapenta <ramono@gmail.com>
2014-04-20 15:13:43 +0100

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 fhpics
 ======
 
-A Statamic plugin to pull photos from 500px.
+A Statamic plugin to pull photos from 500px. The plugin requires the use of a consumer key, which you can obtain by visiting http://500px.com/settings/applications and registering a new application.
+
+## Photo feed
+### Options
+- consumer_key: Your API consumer key, required
+- feature: The type of photo stram to request, it default to user, optional.
+- user: username to pull photos from, required only if feature is set to `user`, `user_friends` or `user_favorites`.
+- count: Amount of photos to request, defaults to 5, optional.
+- image_size: size of the image to be shown, it defaults to 3 (280 x 280), optional
+- sort: Sort photos in the specified order, optional.
+- only: String name of the category to return photos from, optional.
+- exclude: String name of the category to exclude photos by, optional.
+
+### Sample
+	{{ fhpics:photos count="8" consumer_key="<your_key>" user="<username>" }}   
+		{{ photos }}
+		<a href="http://www.500px.com{{ url }}"><img src="{{ image_url }}" alt="{{ name }}"></a>
+		{{ /photos }}  
+	{{ /fhpics:photos }}	
+
+For more information on option values and response format please check [https://github.com/500px/api-documentation/blob/master/endpoints/photo/GET_photos.md]
+
+## Individual photo
+### Options: 
+- consumer_key: Your API consumer key, required
+- id: ID of the image, required.
+- image_size: size of the image to be shown, it defaults to 3 (280 x 280), optional
+- comments: Pull image comments, if omitted comments are not available, optional.
+
+### Sample
+	{{ fhpics:photo id="<photo_id>" image_size="3" comments="1" consumer_key="<your_key>" }}
+	<p><a href="http://www.500px.com{{ url }}"><img src="{{ image_url }}" alt="{{ name }}"></a></p>
+	<p>Description:  {{ description }}</p>
+	<p>{{ camera }} {{ shutter_speed }}s f{{ aperture }} ISO{{ iso }}</p>
+	Comments: 
+	<ul>
+		{{ comments }}
+		<li><a href="http://www.500px.com/{{ user.username }} ">{{ user.fullname }}</a>: {{ body }}</li>
+		{{ /comments }}
+	</ul>
+	{{ /fhpics:photo }}	
+
+For more information on option values for the individual photo, and response format please check [https://github.com/500px/api-documentation/blob/master/endpoints/photo/GET_photos_id.md]
+
+Please note that not all options shown in the documentation have been implemented in this plugin, if you have a request, please get in touch or log an issue here. Thanks.
+
---
+++ pi.fhpics.php
@@ -0,0 +1,106 @@
+<?php 
+
+class Plugin_fhpics extends Plugin 
+{
+	
+	var $meta = array(
+		'name' 		 => 'FHPics',
+		'version'	 => '1',
+		'author'	 => 'Ramon Lapenta',
+		'author_url' => 'http://ramonlapenta.com'
+	);
+
+	public function photos() 
+	{
+		$consumer_key = $this->fetchParam('consumer_key', null);
+		$feature = $this->fetchParam('feature', 'user');
+		$user = $this->fetchParam('user', null);
+
+		$count = $this->fetchParam('count', 5, 'is_numeric');
+		$image_size = $this->fetchParam('image_size', 3, 'is_numeric');
+		$sort = $this->fetchParam('sort', 'created_at');
+		$only = $this->fetchParam('only', null);
+		$exclude = $this->fetchParam('exclude', null);
+
+        $params = "photos?consumer_key=$consumer_key&feature=$feature&username=$user&sort=$sort&rpp=$count&image_size=$image_size&include_store=store_download&include_states=voted";
+	    if(isset($only)) 
+	    {
+	        $params .= '&only='.urlencode($only);
+	    }
+	    if(isset($exclude)) 
+	    {
+	        $params .= '&exclude='.urlencode($exclude);
+	    }
+
+        if ($response = $this->fhp_curl($params)) 
+        {
+            return object_to_array($response);
+        }
+
+        return false;		
+	}
+
+	public function photo() 
+	{
+		$consumer_key = $this->fetchParam('consumer_key', null);
+		$id = $this->fetchParam('id', null);
+		$image_size = $this->fetchParam('image_size', 4, 'is_numeric');
+
+		$comments = $this->fetchParam('comments', null);
+
+	    $params = "photos/$id?consumer_key=$consumer_key&image_size=$image_size";
+	    if(isset($comments)) 
+	    {
+	        $params .= '&comments='.urlencode($comments);
+	    }
+
+        if ($response = $this->fhp_curl($params)) 
+        {
+        	$photo = object_to_array($response);
+            return $photo['photo'];
+        }
+
+        return false;		
+	}
+
+
+
+	// Request query
+    function fhp_curl($params) 
+    {        
+	    $host = 'https://api.500px.com/v1/';
+
+        $request = curl_init($host.$params);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+
+        $contents = curl_exec($request);
+        
+        if ($contents) 
+        {
+            return json_decode($contents);
+        } 
+        else 
+        {
+        	echo "500px requires the CURL library to be installed.";
+        }
+    }
+
+
+}
+
+# Convert results
+function object_to_array($d) 
+{
+    if (is_object($d)) 
+    {
+        $d = get_object_vars($d);
+    }
+    if (is_array($d)) 
+    {
+        return array_map(__FUNCTION__, $d);
+    }
+    else 
+    {
+        return $d;
+    }
+}
